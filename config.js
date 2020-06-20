60c3a974eb0edc576b15d535018b2f8c68ecf9d2
Chelese Jensen <jensenchelese@gmail.com>
2020-06-19 20:51:05 -0700

    Delete Config

--- config.js
+++
@@ -1,8 +0,0 @@
-const data = {
-  username: 'user',
-  password: 'Project123!',
-  AWSAccessKeyId: 'AKIAJF7MUQRCBRTPDVJA',
-  AWSSecretKey: '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'
-}
-
-module.exports = data;
