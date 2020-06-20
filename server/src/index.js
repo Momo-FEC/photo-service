c4c0789b18cdf59012e20c30fc092ccf844b4737
Chelese Jensen <jensenchelese@gmail.com>
2020-06-19 20:48:50 -0700

    Add server reqest

--- server/src/index.js
+++ server/src/index.js
@@ -7,13 +7,26 @@ app.use(bodyParser.json());
 app.listen(3001, () => {
   console.log('Listening on port 3001')
 })
-app.get('/phone', (req, res) => {
-  db.images.findAll({ raw: true })
+
+app.get('/image', (req, res) => {
+  db.images.findAll({ where: {
+    imageId: 3
+  }})
   .then(function (data) {
      res.send(data);
      res.end();
   })
 })
+app.get('/phone', (req, res) => {
+  db.images.findAll({ where: {
+    phoneId: 3
+  }})
+  .then(function (data) {
+     res.send(data);
+     res.end();
+  })
+})
+
 
 
 module.exports = app;
