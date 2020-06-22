837a51ff5efaba3d71845396fc48a753a01a24b4
Chelese Jensen <jensenchelese@gmail.com>
2020-06-22 14:14:04 -0700

    Update testing according to comments

--- spec/database.test.js
+++ spec/database.test.js
@@ -4,34 +4,34 @@ require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 
 afterAll(() => {
   return db.sequelize.close();
-})
+});
 
 
-test('should add data to database', done => {
+test('should check first phone in database', done => {
   return db.images.findAll({ where: {
     phoneId: 1
   }})
-  .then(function (data) {
-    expect(data.length).toBe(7);
-    done();
-  })
-  .catch((err) => {
-    done(err);
-  })
-  done()
-})
+    .then((data) => {
+      expect(data.length).toBe(7);
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+  done();
+});
 
 
 test('should check first image in database', done => {
   return db.images.findAll({ where: {
     imageId: 1
   }})
-  .then(function (data) {
-    expect(data[0].dataValues.imageUrl).toBe('https://fec-photos-carousel.s3-us-west-1.amazonaws.com/FEC+Photos/phone1.webp');
-    done();
-  })
-  .catch((err) => {
-    done(err);
-  })
-  done()
-})
+    .then((data) => {
+      expect(data[0].dataValues.imageUrl).toBe('https://fec-photos-carousel.s3-us-west-1.amazonaws.com/FEC+Photos/phone1.webp');
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+  done();
+});
